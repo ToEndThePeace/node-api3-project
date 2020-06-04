11e1d57e0930dfd0df1348a34528302e7308b175
unknown <bran.ramirez.don@gmail.com>
2020-06-03 20:09:00 -0700

    MVP commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server");
+const port = 8000;
+server.listen(port, () =>
+  console.log(`Server running at http://localhost${port}`)
+);
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+const moment = require("moment");
+
+const userRouter = require("./users/userRouter");
+
+server.use("/api/users", logger, userRouter);
+
+server.get("/", logger, (req, res) => {
+  res.send("Server is running!");
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`-> ${req.method} ${req.originalUrl} ${moment().format()}`);
+  next();
+}
 
 module.exports = server;
