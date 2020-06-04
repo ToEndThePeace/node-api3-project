f22fd2813f8779aac4c6922e36b8670e30840a3a
unknown <bran.ramirez.don@gmail.com>
2020-06-03 20:10:40 -0700

    Added CORS for stretch

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // code away!
 const server = require("./server");
+const cors = require("cors");
+
+server.use(cors());
+
 const port = 8000;
 server.listen(port, () =>
   console.log(`Server running at http://localhost${port}`)
